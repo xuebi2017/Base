296b0c4afc33de5018ba625508cbc8a0bc34456e
zyp <xuebi2013@yeah.net>
2018-11-20 10:08:18 +0800

    base添加

--- base.js
+++ base.js
@@ -1,48 +1,8 @@
-/************************************base***************************************/
-function equals(x, y) {
-    var f1 = x instanceof Object
-    var f2 = y instanceof Object
-    if(!f1 || !f2) {
-        return x === y
-    }
-    if(Object.keys(x).length !== Object.keys(y).length) {
-        return false
-    }
-    var indexofX = Object.keys(x)
-    for(var index of indexofX) {
-        count++
-        var f3 = x[index] instanceof Object
-        var f4 = y[index] instanceof Object
-        if(f3 && f4) {
-            equals(x[index], y[index])
-        }else if(x[index] !== y[index]) {
-            return false
-        }
-    }
-    return true
-}
-
-function promiseJSON({url, data = '', type = 'GET', contentType, async = true}) {
-    // let {url, data = '', type, contentType = 'text/html', async = true} = options
-    return new Promise((resolve, reject) => {
-        var xhr = new XMLHttpRequest();
-        xhr.open(type, url, async);
-        contentType && xhr.setRequestHeader('content-type', contentType)
-        xhr.onreadystatechange = function(res) {
-            if(xhr.readyState === 4) {
-                if(xhr.status === 200) {
-                    // console.log('xhr', xhr)                    
-                    resolve.call(undefined,JSON.parse(xhr.responseText))
-                }else {
-                    reject.call(undefined, xhr)
-                }
-            }
-        };
-        xhr.send(data);
-    })
-}
-
 /*****************************************String**********************************/
+/**
+ * 输入：占位符中需要替换成的字符串序列
+ * 输出：替换完成的结果
+ */
 String.prototype.format = function() {
     let length = arguments.length
     let str = this + ''
@@ -51,9 +11,12 @@ String.prototype.format = function() {
     }
     return str
 }
-
-
 /*****************************************Number***********************************/
+/**
+ * 
+ * @param {*} length 需要约成小数点后面length位
+ * @returns 返回的是四舍五入的结果，该方法可以弥补javaScript自带的toFixed方法中对于四舍五入处理不精确的问题（js中的toFixed基本遵循四舍六入五成双的原则）
+ */
 Number.prototype.toFixed = function(length)
 {
     var carry = 0; //存放进位标志
@@ -120,3 +83,122 @@ function getHashData2(rows, keys) {
     return hash 
 }
 
+function equals(x, y) {
+    var f1 = x instanceof Object
+    var f2 = y instanceof Object
+    if(!f1 || !f2) {
+        return x === y
+    }
+    if(Object.keys(x).length !== Object.keys(y).length) {
+        return false
+    }
+    var indexofX = Object.keys(x)
+    for(var index of indexofX) {
+        count++
+        var f3 = x[index] instanceof Object
+        var f4 = y[index] instanceof Object
+        if(f3 && f4) {
+            equals(x[index], y[index])
+        }else if(x[index] !== y[index]) {
+            return false
+        }
+    }
+    return true
+}
+
+/************************************Array*****************************/
+//ES5写法
+/**
+ *
+ * @param {*} array, 待去重的数组
+ * @returns 输出去重后的数组·
+ */
+function unique(array) {
+    var res = array.filter(function (item, index, array) {
+        return array.indexOf(item) === index;
+    })
+    return res;
+}
+//ES6写法, a是待去重的数组，b是去重后的数组
+var b = [...new Set(a)]
+
+/************************************Date*****************************/
+/**
+ * 
+ * @param {日期} strDate, 格式：2018-01-01
+ * @returns 返回的是输入日期是星期几，string, 如：'星期一' 
+ */
+var getDay = function(strDate) {
+    let days = ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+    let str = strDate.replace(/-/g, '/');
+    let i = new Date(str).getDay();
+    return days[i]
+}
+
+//用javascript取得某一年的第一个星期一的日期 
+/**
+ *
+ * @param {年份} year, Number类型，如：2018
+ * @returns 返回的是日期，GMT格式：Mon Feb 05 2018 00:00:00 GMT+0800 (中国标准时间)
+ */
+function get(year) {
+    var d = new Date(year, 1, 1);
+    var day = d.getDay(); //获取1月1号是星期几
+    d.setDate((8 - day) % 7 + 1);
+    return d;
+}
+
+//求前 n 天或者后 n 天的日期
+/**
+ * 
+ * @param {前n天或者后n天，如果是后n天，n是正数；如果是前n天，n是负数。}} n 
+ * @returns 返回的是前n天或者后n天的日期，日期格式是2018/11/16
+ */
+var showdate = function(n){
+    var d = new Date();
+    d.setDate(d.getDate()+n);
+    //或者 d = d.getFullYear() + "-" +  (d.getMonth()+1) + "-" + d.getDate();
+    d = d.toLocaleDateString().replace(/[\u4e00-\u9fa5]/g,'-').replace(/-$/,'')    
+    return d;
+}
+
+
+// Date.prototype.getDayOfWeek = function(strDate) {
+//     let days = ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+//     let str = strDate.replace(/-/g, '/');
+//     let i = new Date(str).getDay();
+//     return days[i]
+// }
+
+
+/***********************************tools****************************/
+/**
+ * 
+ * @param {需要格式化的金额字符串} cashStr 
+ * @returns 输出格式化后的金额
+ */
+let formatCash = function(cashStr) {
+    return cashStr.split('').reverse().reduce((prev, cur, index) => {
+        return ((index%3) ? cur : (cur+',')) + prev
+    })
+}
+
+function promiseJSON({url, data = '', type = 'GET', contentType, async = true}) {
+    // let {url, data = '', type, contentType = 'text/html', async = true} = options
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.open(type, url, async);
+        contentType && xhr.setRequestHeader('content-type', contentType)
+        xhr.onreadystatechange = function(res) {
+            if(xhr.readyState === 4) {
+                if(xhr.status === 200) {
+                    // console.log('xhr', xhr)                    
+                    resolve.call(undefined,JSON.parse(xhr.responseText))
+                }else {
+                    reject.call(undefined, xhr)
+                }
+            }
+        };
+        xhr.send(data);
+    })
+}
